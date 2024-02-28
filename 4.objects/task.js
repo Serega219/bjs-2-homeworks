78185f12c9984abffcc8ed1ed42eab2b9b8f9b54
Serega219 <94173329+Serega219@users.noreply.github.com>
2024-02-28 13:11:03 +0300

    New2.js

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,49 +1,35 @@
 function Student(name, gender, age) {
-
-  
-  
   this.name = name;
   this.gender = gender;
   this.age = age;
   this.marks = [];
 
- 
-  
   this.setSubject = function(subjectName) {
     this.subject = subjectName;
   };
- 
-  
-  
+
   this.addMarks = function(...marksToAdd) {
     if (!this.marks) {
       console.log("Error: Student has been expelled.");
-    return;
-  }
-  this.marks.push(...marksToAdd);
+      return;
+    }
+    this.marks.push(...marksToAdd);
   };
-  
 
+  this.getAverage = function() {
+    if (!this.marks || !this.marks.length) {
+      return 0;
+    }
+    const sum = this.marks.reduce((acc, curr) => acc + curr);
+    return sum / this.marks.length;
+  };
 
-
-
-this.getAverage = function() {
-  if (!this.marks || !this.marks.lengh) {
-    return 0;
-  }
-  const sum = this.marks.reduce((Acc, curr) => acc + curr);
-  return sum / this.marks.lenght;
-};
-
-
-
-this.exclude = function(reason) {
-  delete this.subject;
-  delete this.marcs;
-  this.excluded = reason;
-};
+  this.exclude = function(reason) {
+    delete this.subject;
+    delete this.marks;
+    this.excluded = reason;
+  };
 }
-
 
 let student1 = new Student("Васелиса","женский",19);
 student1.setSubject("Algebra");
@@ -56,4 +42,4 @@ let student2 = new Student("Артем","мужской",25);
 student2.setSubject("Geometry");
 student2.exclude('плохая учеба')
 console.log(student2)
-console.log(student2.getAverage());
+console.log(student2.getAverage()); 
